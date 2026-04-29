cf2b12790759fa6f4c8ec8e7813929682348aaeb
cs-dev-09 <csramu38@gmail.com>
2026-04-29 15:58:50 +0530

    Add files via upload

---
+++ cs_moonshine_prop/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CSDev'
+description 'Moonshine Prop'
+version '1.0.0'
+
+files {
+    'stream/cs_moonshine_prop.ytyp'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/cs_moonshine_prop.ytyp'
+
+
+
+
+
